439308ad673223528c7a7ffe33d0dbbc04ed33ab
Yusuke Nagano <s20c1085aa@s.chibakoudai.jp>
2022-01-11 11:46:28 +0000

    add a file

--- scripts/twice.py
+++ scripts/twice.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
-
+rospy.init_node('twice')
+pub = rospy.Publisher('twice_up',Int32,queue_size=1)
+rate = rospy.Rate (10)
 n = 0
-
 def cb(message):
-	global n
-	n = message.data*2
-
-if __name__== '__main__':
+	pub.publish(message.data*2)
+if __name__ =='__main__':
 	rospy.init_node('twice')
-	sub = rospy.Subscriber('count_up', Int32, cb)
-	pub = rospy.Publisher('twice', Int32, queue_size=1)
-	rate = rospy.Rate(10)
-	while not rospy.is_shutdown():
-		pub.publish(n)
-		rate.sleep()
+	sub = rospy.Subscriber('count_up' , Int32, cb)
+	rospy.spin()
